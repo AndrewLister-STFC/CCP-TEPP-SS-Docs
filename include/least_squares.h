520192bad76a993ca5afbb21303a9cf16a0f700f
Andrew Lister <andrew.lister@stfc.ac.uk>
2026-07-01 16:46:06 +0100

    Add first draft of doxygen docs

--- include/least_squares.h
+++ include/least_squares.h
@@ -4,12 +4,19 @@
 #ifndef LEAST_SQUARES_H
 #define LEAST_SQUARES_H
 
+/// @brief x, y datapoints for least squares regression
 typedef struct {
-    double *x;
+    /// The input values
+    double *x; 
+    /// The output values
     double *y;
-    int n;
+    /// The number of data points
+    int n; 
 } Dataset;
 
+/// @brief Read in some parameter data to the Dataset struct
+/// @param filename Path to the file to read
+/// @return A populated Dataset with our data points
 Dataset read_data(const char *filename);
 void solve_least_squares(const Dataset *data, double *slope, double *intercept);
 void print_solution(double slope, double intercept);
